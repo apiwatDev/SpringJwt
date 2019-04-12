5d95c9c6d4b8167857ed85b2de2fb1466cb7ae0d
DESKTOP-0M5DEBU\axamr <axamrffluke@outlook.com>
2019-04-12 23:24:34 +0700

    first commit jwt

---
+++ src/main/java/com/apiwatDev/jwt/SpringSeurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.apiwatDev.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeurityJwtApplication.class, args);
+	}
+
+}
